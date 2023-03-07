35650036f22b24e053e61f35d33408258be64d33
Jefferson Dada <125513220+xnaps@users.noreply.github.com>
2023-03-07 19:23:21 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+/*aqui estamos criando um array de informaçoes o array e um conjunto
+ de informações agrupadas em linha*/  
+ let valores [7.7, 8.5, 5.5]
+/*aqui estamos acessando os indices de um array*/ 
+console.log(valores[0], valores[3])
+console.log(valores)
+/*aqui estamos adicionando um novo elemento ao array usando o indice*/
+valores(4) = 10
+console.log(valores.length)
+/*o push é uma função de array e é ultilzada quando queremos adicionar um conjunto de
+Informações ao array*/
+valores.push((1:3), false, mall, 'teste') 
+console.log(valores)
+/*mesmo podendo adicionar diversos tipos difentes devemos prezar
+dentro de un array devemos prezar por array's homogeneos ou seja
+do mesmo tipo primitivo*/ 
+valores.pop()
+console.log(valores)
+/*o pop() remove o ultimo elemento do array por vez */
+/*o delete renove o elemento do array pelo indice escolhido pelo usuario */
+delete valores[0]
+console.log(typeof valores)
+console.log(valores) 
+/*podemos tambem fazer uso dos arrays para criar operações aritimeticas*/
+let calculo [10,15)
+let resultado- calculo[0] + calculo[1]
+console.log(resultado)
